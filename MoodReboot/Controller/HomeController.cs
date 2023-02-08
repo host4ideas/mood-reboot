4aa4a1df180fdce266a597993063881f18d5aed2
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-02-08 14:27:55 +0100

    sql updates

---
+++ MoodReboot/Controller/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoodReboot.Controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
