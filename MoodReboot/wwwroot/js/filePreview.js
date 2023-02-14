f893b8e131d09bdc882fa1e95c7201faf4b9231c
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-02-14 14:21:11 +0100

    filePReview working

--- MoodReboot/wwwroot/js/filePreview.js
+++ MoodReboot/wwwroot/js/filePreview.js
@@ -6,8 +6,12 @@ pdfjsLib.GlobalWorkerOptions.workerSrc = './js/pdfjs-3.3.122-dist/build/pdf.work
 $("#hidden-file").on("change", function (e) {
     var file = e.target.files[0]
 
-    if (file.type == "application/pdf") {
+    // Clean previews
+    $('#imgImagePreview').empty();
+    $('#canvasPdfPreview').empty();
+    $("#tableDataPreview").empty();
 
+    if (file.type == "application/pdf") {
         try {
             var fileReader = new FileReader();
             fileReader.onload = function () {
@@ -19,7 +23,7 @@ $("#hidden-file").on("change", function (e) {
                     pageRendering = false,
                     pageNumPending = null,
                     scale = 0.8,
-                    canvas = document.getElementById('the-canvas'),
+                    canvas = document.getElementById('canvasPdfPreview'),
                     ctx = canvas.getContext('2d');
 
                 /**
@@ -98,8 +102,7 @@ $("#hidden-file").on("change", function (e) {
                  */
                 pdfjsLib.getDocument({ data: pdfData }).promise.then(function (pdfDoc_) {
                     pdfDoc = pdfDoc_;
-                    document.getElementById('page_count').textContent = pdfDoc.numPages;
-
+                    ; $("#page_count").text(pdfDoc.numPages);
                     $("#previewContent").removeClass("hidden");
 
                     // Initial/first page rendering
@@ -112,56 +115,59 @@ $("#hidden-file").on("change", function (e) {
             $("#previewContent").addClass("hidden");
         }
     }
-    //$('#excel_data').empty("");
 
     if (file.type == "image/jpeg" || file.type == "image/png" || file.type == "image/webp" || file.type == "image/gif") {
-        document.getElementById("img1").style.display = "block";
-
         var reader = new FileReader();
         reader.onload = function () {
-            var output = document.getElementById('img1');
-            output.src = reader.result;
+            $('#imgImagePreview').attr("src", reader.result);
+            $("#previewImage").removeClass("hidden");
         };
         reader.readAsDataURL(e.target.files[0]);
-
-        //  $('#img1').attr("src", "");
     }
+
     if (file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
         var reader = new FileReader();
-
         reader.readAsArrayBuffer(e.target.files[0]);
-
         reader.onload = function (e) {
 
             var data = new Uint8Array(reader.result);
-
             var work_book = XLSX.read(data, { type: 'array' });
-
             var sheet_name = work_book.SheetNames;
-
             var sheet_data = XLSX.utils.sheet_to_json(work_book.Sheets[sheet_name[0]], { header: 1 });
 
             if (sheet_data.length > 0) {
-                var table_output = '<table class="table table-striped table-bordered">';
+                var table_output = '<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">';
 
                 for (var row = 0; row < sheet_data.length; row++) {
 
-                    table_output += '<tr>';
+                    table_output += '<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">';
 
                     for (var cell = 0; cell < sheet_data[row].length; cell++) {
 
                         if (row == 0) {
-                            table_output += '<th>' + sheet_data[row][cell] + '</th>';
+                            table_output += '<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">' + sheet_data[row][cell] + '</th>';
                         }
                         else {
-                            table_output += '<td>' + sheet_data[row][cell] + '</td>';
+                            table_output += '<td class="px-6 py-4">' + sheet_data[row][cell] + '</td>';
                         }
                     }
                     table_output += '</tr>';
                 }
                 table_output += '</table>';
-                document.getElementById('excel_data').innerHTML = table_output;
+                $("#tableDataPreview").removeClass("hidden").html(table_output);
             }
         }
     }
+});
+
+$("#toggleImgPreview").click(function () {
+    $("#imgImagePreview").toggleClass("hidden");
+});
+
+$("#toggleExcelPreview").click(function () {
+    $("#tableDataPreview").toggleClass("hidden");
+});
+
+$("#togglePdfPreview").click(function () {
+    $("#previewContent").toggleClass("hidden");
 });
