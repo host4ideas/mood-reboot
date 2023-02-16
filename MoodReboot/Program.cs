635b12c5c7cfdc1e1d7dff48ded6d61824d95297
host4ideas <72515482+host4ideas@users.noreply.github.com>
2023-02-16 14:23:52 +0100

    mvc EF

--- MoodReboot/Program.cs
+++ MoodReboot/Program.cs
@@ -10,13 +10,26 @@ builder.Services.AddTransient<IStreamFileUploadService, StreamFileUploadLocalSer
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.UseRouting();
+
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}"
     );
-
-app.MapHub<ChatHub>("/chatHub");
 
 app.Run();
