ab8707f30b35a7f4b5940cd8465507e2c45e7444
host4ideas <felixmb99@hotmail.es>
2023-02-27 12:17:23 +0100

    updated database, added procedures

--- MoodReboot/Data/MoodRebootSqlContext.cs
+++ MoodReboot/Data/MoodRebootSqlContext.cs
@@ -8,5 +8,9 @@ namespace MoodReboot.Data
         public MoodRebootSqlContext(DbContextOptions<MoodRebootSqlContext> options) : base(options) { }
         public DbSet<Message> Messages { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Center> Centers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<ContentGroup> ContentGroups { get; set; }
+        public DbSet<Content> Contents { get; set; }
     }
 }
---
+++ MoodReboot/Interfaces/IRepositoryCenters.cs
@@ -0,0 +1,14 @@
+﻿using MoodReboot.Models;
+
+namespace MoodReboot.Interfaces
+{
+    public interface IRepositoryCenters
+    {
+        public List<Center> GetAllCenters();
+        public List<Center> GetUserCenters(int id);
+        public Task<Center?> FindCenter(int id);
+        public Task CreateCenter(string email, string name, string address, string file, string telephone);
+        public Task UpdateCenter(Center center);
+        public Task DeleteCenter(int id);
+    }
+}
