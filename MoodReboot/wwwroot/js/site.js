235d2c248d8001eb7fd6241a0b825a0dc083cc13
host4ideas <felixmb99@hotmail.es>
2023-02-27 12:17:54 +0100

    navbar design finished

--- MoodReboot/wwwroot/js/site.js
+++ MoodReboot/wwwroot/js/site.js
@@ -12,6 +12,26 @@ $(function () {
     });
 });
 
+// Scroll to top button
+// Get the button 
+let mybutton = document.getElementById('btn-back-to-top'); // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction(); };
+function scrollFunction() {
+    console.log("test")
+    if (document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20) {
+        mybutton.style.display =
+            'flex';
+    } else {
+        mybutton.style.display = 'none';
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', backToTop);
+function backToTop() {
+    document.body.scrollTop = 0; document.documentElement.scrollTop = 0;
+}
+
 // Theme toggle
 var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
 var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');         // Change the icons inside the button based on previous settings
